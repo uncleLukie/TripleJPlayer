959bb0753bd7fe9d5e6e91773bcf92e1a442ca9e
UncleLukie <lukehewitt@smashit.games>
2023-02-12 14:40:01 +1000

    app now gracefully ends thread and closes selenium chrome driver

--- start.py
+++ start.py
@@ -9,23 +9,31 @@ import discordpresence
 url = "https://www.abc.net.au/listenlive/triplej"
 client_id = "1074173586262196254"
 
+stop_thread = False
+fetcher = songfetcher.SongFetcher(url)
+
+
+def run_thread():
+    global stop_thread, fetcher
+    while not stop_thread:
+        time.sleep(random.uniform(2, 4))
+        title, artist, album = fetcher.get_song_info()
+        if title and artist and album:
+            print(f"Now playing: {title} by {artist} in release {album}")
+            # drpc.update(title, artist, album)
+        else:
+            print("Unable to retrieve song info")
+
+
+def stop_thread_on_exit():
+    global stop_thread
+    stop_thread = True
+    fetcher.driver.close()
+    fetcher.driver.quit()
+
+
 if __name__ == "__main__":
-
-    fetcher = songfetcher.SongFetcher(url)
     # drpc = discordpresence.DiscordPresence(client_id, token)
-
-    stop_thread = False
-    def run_thread():
-        global stop_thread
-        while not stop_thread:
-            time.sleep(random.uniform(2, 4))
-            title, artist, album = fetcher.get_song_info()
-            if title and artist and album:
-                print(f"Now playing: {title} by {artist} in release {album}")
-                # drpc.update(title, artist, album)
-            else:
-                print("Unable to retrieve song info")
-
 
     t = threading.Thread(target=run_thread)
     t.daemon = True
@@ -34,9 +42,6 @@ if __name__ == "__main__":
     webview.create_window('DiscordRichJPlayer', url, width=360, height=611)
     webview.start()
 
-    while not webview.closed:
-        pass
+    atexit.register(stop_thread_on_exit)
 
-    stop_thread = True
-    del fetcher
     exit(0)
